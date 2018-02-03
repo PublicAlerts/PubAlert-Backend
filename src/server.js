db652db837e76143337a155dbd2c7d53c224024d
Maks <pvl1720@gmail.com>
2018-02-03 13:32:31 -0800

    server added

--- src/server.js
+++ src/server.js
@@ -10,7 +10,18 @@ let MongoClient = require('mongodb').MongoClient;
 app.use(morgan('dev'))
 app.use(require('./router/alerts-route.js'));
 
+let usePort = process.env.PORT || 3000;
 
+app.listen(usePort, () => {
+  console.log('__SERVER_UP__', usePort)
+})
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/passdb", function(err, db) {
+  if(!err) {
+    console.log("We are connected to Mongo");
+  }
+});
 
 
 
