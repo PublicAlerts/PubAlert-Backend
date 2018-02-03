af87973e61761ff4d5dac56537c7a4e4dd23b6e5
Maks <pvl1720@gmail.com>
2018-02-03 13:45:41 -0800

    POST route for alerts

--- src/router/alerts-route.js
+++ src/router/alerts-route.js
@@ -5,6 +5,13 @@ const router = express.Router();
 const jsonParser = require('body-parser').json();
 const Alerts = require('../lib/model/alerts.js');
 
+router.post('/api/alerts', jsonParser, (req, res, next) => {
+  console.log('HERE IN POST');
+  let newAlerts = new Alerts(req.body);
+  newAlerts.save()
+    .then(data => res.send(data)) //sending data back
+    .catch(err => next({statusCode: 500, message: 'Unable to record your alert', error: err}));
 
+});
 
 module.exports = router;
