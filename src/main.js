c9d12c660c466dc825f4fd6bb0c5d4d295d685fe
Maks <pvl1720@gmail.com>
2018-02-03 13:32:03 -0800

    building out

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-import * as server from './lib/server.js'
-
-server.start()
+// import * as server from './lib/server.js'
+//
+// server.start()
--- src/server.js
+++ src/server.js
@@ -3,11 +3,15 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+let MongoClient = require('mongodb').MongoClient;
 
 app.use(morgan('dev'))
 app.use(require('./router/alerts-route.js'));
 
 
-let usePort = process.env.PORT || 3000;
+
+
 
 module.exports = app;
