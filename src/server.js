55c39cad024e27efb75a95d3d24a373dee63794c
Maks <pvl1720@gmail.com>
2018-02-04 16:54:52 -0800

    move error handling outta server into a separate file

--- src/server.js
+++ src/server.js
@@ -22,32 +22,8 @@ mongoose.connect('mongodb://passdb:FettyWap1738yeah@ds225078.mlab.com:25078/pass
 
 app.use(morgan('dev'))
 app.use(require('./router/alerts-route.js'));
+app.use(require('./error-handle.js'));
 
-
-app.use((err, req, res, next) => {
-
-  console.error(err.message.toLowerCase());
-
-  if(err.message.includes('validation failed'))
-    return res.sendStatus(400);
-
-  if(err.message.includes('unauthorized'))
-    return res.sendStatus(400);
-
-  if(err.message.includes('jwt malformed'))
-    return res.sendStatus(400);
-
-  if(err.message.includes('invalid token'))
-    return res.sendStatus(401);
-
-  if(err.message.includes('objectid failed'))
-    return res.sendStatus(404);
-
-  if(err.message.includes('duplicate key'))
-    return res.sendStatus(409);
-
-  res.sendStatus(500);
-});
 
 
 module.exports = app;
